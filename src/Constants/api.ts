55021e3b298d98df9e413bc0de5b591e40de9a8d
Dias <30836762+madhawadias@users.noreply.github.com>
2024-05-09 20:19:19 +0530

    changed api endpoint - tmp

--- src/Constants/api.ts
+++ src/Constants/api.ts
@@ -1,4 +1,5 @@
-export const BASEURL = "https://api.spsfulfillment.com/api/";
+export const BASEURL = "https://6cdb-16-171-202-3.ngrok-free.app/api/";
+//export const BASEURL = "https://api.spsfulfillment.com/api/";
 //export const BASEURL = "http://localhost:5000/api/";
 export const USERURL = "user"
 export const INQUIRYURL = "inquiry"
